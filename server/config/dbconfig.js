a497b029a1741d907e8d28a0b3d9aceb9ee63342
Jang-JinYoung <jinyoung_8965@naver.com>
2021-09-06 01:19:10 +0900

    db 수정
    db 수정

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'uws7-214.cafe24.com',
-    user     : 'jinyoung4892',
-    password : 'aa75287528!',
-    database : 'jinyoung4892'
+    host     : '',
+    user     : '',
+    password : '',
+    database : ''
 });
 
 module.exports = connection;
